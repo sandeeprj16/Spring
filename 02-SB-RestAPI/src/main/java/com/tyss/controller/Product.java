7d731a78bfdc47cc612981753c9a9dd3510ab603
vishal <sj537376@gmail.com>
2026-02-28 04:00:25 +0530

    added

--- 02-SB-RestAPI/src/main/java/com/tyss/controller/Product.java
+++ 02-SB-RestAPI/src/main/java/com/tyss/controller/Product.java
@@ -1,10 +1,18 @@
 package com.tyss.controller;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int pid;
 	private String pname;
 	
-	
+	public Product() {}
 	
 	@Override
 	public String toString() {
--- 02-SB-RestAPI/src/main/java/com/tyss/controller/ProductController.java
+++ 02-SB-RestAPI/src/main/java/com/tyss/controller/ProductController.java
@@ -1,5 +1,11 @@
 package com.tyss.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,29 +21,65 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-	@GetMapping("/fetch")
-	public String fetch(@RequestParam (name="pid")int id, @RequestParam(name="pname") String name){
-		return "Product id- "+id+ ", Product name- "+name;
+//	@GetMapping("/fetch")
+//	public String fetch(@RequestParam (name="pid")int id, @RequestParam(name="pname") String name){
+//		return "Product id- "+id+ ", Product name- "+name;
+//	}
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping("/all")
+	public List<Product> fetch(){
+		List<Product > products=productRepository.findAll();
+		return products;
+	}
+	@GetMapping("/byId")
+	public Product fetchById(@RequestParam Integer pid) {
+		return productRepository
+				.findById(pid)
+				.orElseThrow(()-> new RuntimeErrorException(null,"Product not found"));
 	}
 	@PostMapping("/save")
 	public Product save(@RequestBody Product product) {
 		// TODO Auto-generated method stub
-		System.out.println(product);
-		return product;
+		Product savedProduct=productRepository.save(product);
+		return savedProduct;
 	}
-	@DeleteMapping("/delete/{id}/name/{pname}")
+	
+	@PutMapping("/update/pid/{pid}")
+	public String update(@PathVariable Integer pid, @RequestBody Product product) {
+		
+		Optional<Product> opt=productRepository.findById(pid);
+		
+		if(opt.isPresent()) {
+		Product dbproduct = opt.get();
+		dbproduct.setPid(product.getPid());
+		dbproduct.setPname(product.getPname());
+		
+		productRepository.save(dbproduct);
+		return "Product update successfully";
+		}
+		return "project not found";
+		
+		// TODO Auto-generated method stub
+//		System.out.println(brand);System.out.println(token);
+//		//return "BrandName- "+brand+" Token-"+token ;
+//		
+//		return "Product Updated: "+brand+" "+token;
+	}
+	@DeleteMapping("/delete/{id}")
 	private String delete( @PathVariable (name="id")int pid,@PathVariable String pname) {
 		// TODO Auto-generated method stub
-		return "delete successful: "+pid +" "+pname;
-	}
-	@PutMapping("/update")
-	private String Update(@RequestHeader (name="pbrand")String brand, @RequestHeader (name="ptoken")String token) {
-		// TODO Auto-generated method stub
-		System.out.println(brand);System.out.println(token);
-		//return "BrandName- "+brand+" Token-"+token ;
 		
-		return "Product Updated: "+brand+" "+token;
+		Optional<Product> opt = productRepository.findById(pid);
+		if (opt.isPresent()) {
+			productRepository.deleteById(pid);
+			return "product deleted";
+		}
+		return "Product not found";
 	}
+	
 	@PatchMapping("/patch")
 	private String patch() {
 		// TODO Auto-generated method stub
